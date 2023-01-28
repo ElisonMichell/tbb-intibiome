a785e4624adf7becba44583aac6bef0a046baa64
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-28 14:36:42 -0300

    feat(home): add banner section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
+import banner from "/src/assets/banner-hero.png"
 
 function App() {
   return (
     <div>
       <Header />
+      <img src={banner} alt="banner hero" className="banner"/>
     </div>
   )
 }
