ef178fa8c8432ad8c503b796bd948eb06f7ec0b8
Elison Michell <dev.elisonmichell@gmail.com>
2023-01-28 13:28:43 -0300

    feat(header): add dropdown menu
    - add dropdown component to header
    - add hover-based fade in and out effect
    - add min height of 100vh to body element in '_reset.scss'
    - add hover color variable to '_variables.scss'
    - add translate-bottom, hover-color, horizontal-padding, transition,
      position, absolute, relative and fixed mixins

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,18 +8,55 @@ function Header() {
   return(
     <header>
       <div className='header-bar'>
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" className='logo' />
       </div>
+
       <nav>
-        <ul>
+        <ul className='navigation'>
           <li>
-            <button>about us <img src={down} alt="down icon" /></button>
+            <button>about us <img src={down} alt="down icon" height={14} /></button>
+            <ul className='drop-down'>
+              <li>
+                <button>brand philosophy</button>
+              </li>
+              <li>
+                <button>product technology</button>
+              </li>
+            </ul>
           </li>
           <li>
-            <button>our products  <img src={down} alt="down icon" /></button>
+            <button>our products  <img src={down} alt="down icon" height={14} /></button>
+            <ul className='drop-down'>
+              <li>
+                <button>all products</button>
+              </li>
+              <li>
+                <button>intibiome wellness</button>
+              </li>
+              <li>
+                <button>intibiome active</button>
+              </li>
+              <li>
+                <button>intibiome agecare</button>
+              </li>
+            </ul>
           </li>
           <li>
-            <button>intimate health  <img src={down} alt="down icon" /></button>
+            <button>intimate health  <img src={down} alt="down icon" height={14} /></button>
+            <ul className='drop-down'>
+              <li>
+                <button>article 1</button>
+              </li>
+              <li>
+                <button>article 2</button>
+              </li>
+              <li>
+                <button>article 3</button>
+              </li>
+              <li>
+                <button>faq</button>
+              </li>
+            </ul>
           </li>
           <li>
             <button>contact us</button>
